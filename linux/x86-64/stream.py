f1d04db1c8c2ac6c47db194e5364b50bff8d1b3e
olym <olym@acrcloud.com>
2016-08-27 15:20:57 +0800

    get url from bucket

--- linux/x86-64/stream.py
+++ linux/x86-64/stream.py
@@ -16,6 +16,7 @@ import socket
 import hmac
 import subprocess
 import multiprocessing
+import requests
 from xml.dom import minidom
 import acrcloud_stream_decode
 
@@ -55,7 +56,7 @@ class LiveStreamWorker():
             self.setDaemon(True)
             self._config = config
             self._stream_url = stream_info['url']
-            self._stream_acrid = stream_info['acrc_id']
+            self._stream_acrid = stream_info['acr_id']
             self._program_id = stream_info.get('program_id', -1)
             self._worker_queue = worker_queue
             self._fp_interval = self._config.get('fp_interval_sec', 2)
@@ -340,19 +341,29 @@ class LiveStreamClient():
 
 def get_remote_config(config):
     try:
+        bucket_name = config['bucket_name']
+        account_access_key = config['access_key']
+        account_access_secret = config['access_secret']
+        requrl = "https://api.acrcloud.com/v1/"+bucket_name+"/eu_test/channels"
+        http_method = "GET"
+        http_uri = "/v1/buckets/"+bucket_name+"/channels"
+        signature_version = "1" 
         timestamp = time.time()
-        signature = base64.b64encode(hmac.new(config['access_secret'], config['access_key']+str(timestamp), digestmod=hashlib.sha1).hexdigest())
-        values = {'access_key' : config['access_key'], 'timestamp': timestamp,
-            'sign' : signature}
-        data = urllib.urlencode(values)
-        url = 'http://console.acrcloud.com/service/channels?'+data
-        req = urllib2.Request(url)
-        response = urllib2.urlopen(req)
-        recv_msg = response.read()
-        json_res = json.loads(recv_msg)
-        if json_res['response']['status']['code'] == 0:
-            config['streams'] = json_res['response']['metainfos']
-        logging.getLogger('acrcloud_stream').info(recv_msg)
+
+        string_to_sign = http_method+"\n"+http_uri+"\n"+account_access_key+"\n"+signature_version+"\n"+str(timestamp)
+        sign = base64.b64encode(
+                        hmac.new(account_access_secret, string_to_sign, digestmod=hashlib.sha1)
+                                .digest())
+
+        headers = {'access-key': account_access_key, 'signature-version': signature_version, 'signature': sign, 'timestamp':timestamp}
+
+        r = requests.get(requrl, headers=headers, verify=True)
+        r.encoding = "utf-8"
+        if r.status_code == 200:
+            recv_msg = r.text
+            json_res = json.loads(recv_msg)
+            config['streams'] = json_res['items']
+            logging.getLogger('acrcloud_stream').info(recv_msg)
     except Exception, e:
         logging.getLogger('acrcloud_stream').error('get_remote_config : %s' % str(e))
         sys.exit(-1)
@@ -394,12 +405,13 @@ def parse_config():
         else:
             init_log(logging.ERROR, log_file)
 
-        config['access_key'] = init_config['access_key']
-        config['access_secret'] = init_config['access_secret']
+        config['access_key'] = init_config['console_access_key']
+        config['access_secret'] = init_config['console_access_secret']
         config['remote'] = init_config.get('remote')
         config['restart_interval_minute'] = init_config.get('restart_interval_minute', 0)
         config['is_run_with_watchdog'] = init_config.get('is_run_with_watchdog', 0)
         config['upload_timeout_sec'] = init_config.get('upload_timeout_sec', 10)
+        config['bucket_name'] = init_config.get('bucket_name')
         if init_config.get('remote'):
             get_remote_config(config)
         else:
